fd8266bbd989a02e72316580dbac35fc1bc8e426
Iaroslav Bulimov <iaroslavbulimov@192.168.100.4>
2026-02-25 19:19:50 +0300

    Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -2,29 +2,15 @@ from playwright.sync_api import  expect, Page
 import re
 import pytest
 
+from pages.courses_list_page import CoursesListPage
+
+
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page) -> None:
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-    # дополнительно убеждаемся что редирект произошел, в урле есть /#/courses
-    expect(chromium_page_with_state).to_have_url(re.compile(r".*/#/courses.*"))
-
-    # проверяем наличие и текст заголовка "Courses"
-    courses_toolbar_title_txt = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-    expect(courses_toolbar_title_txt).to_be_visible()
-    expect(courses_toolbar_title_txt).to_have_text("Courses")
-
-    # проверяем наличие и текст блока "There is no results"
-    courses_view_title_txt = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-    expect(courses_view_title_txt).to_be_visible()
-    expect(courses_view_title_txt).to_have_text("There is no results")
-
-    # проверяем наличие и видимость иконки пустого блока
-    courses_list_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-    expect(courses_list_icon).to_be_visible()
-
-    # проверяем наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
-    courses_description_txt = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-    expect(courses_description_txt).to_be_visible()
-    expect(courses_description_txt).to_have_text("Results from the load test pipeline will be displayed here")
-
+def test_empty_courses_list(courses_list_page: CoursesListPage) -> None:
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
+    courses_list_page.check_visible_create_course_button()
