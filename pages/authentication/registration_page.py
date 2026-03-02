0432edcb18a6a52518c3f76d857a74e72a9d96c1
Iaroslav Bulimov <iaroslavbulimov@192.168.100.4>
2026-03-02 18:14:14 +0300

    Добавление автотеста test_navigate_from_authorization_to_registration

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,6 +1,9 @@
+import re
+
 from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.link import Link
 from pages.base_page import BasePage
 
 
@@ -10,6 +13,12 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login Link')
 
     def click_registration_button(self):
         self.registration_button.click()
+
+    def click_login_link(self):
+        self.login_link.click()
+        # Добавили проверку
+        self.check_current_url(re.compile(".*/#/auth/login"))
