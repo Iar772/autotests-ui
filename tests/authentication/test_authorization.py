b2ae5329099f1a99d17ea281d96d5cd98005be6e
Iaroslav Bulimov <iaroslavbulimov@192.168.100.4>
2026-03-02 18:09:11 +0300

    Добавление автотеста test_successful_authorization

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -8,6 +8,30 @@ from pages.dashboard.dashboard_page import DashboardPage
 @pytest.mark.regression
 @pytest.mark.authorization
 class TestAuthorization:
+
+    def test_successful_authorization(
+        self,
+        login_page: LoginPage,
+        dashboard_page: DashboardPage,
+        registration_page: RegistrationPage
+    ):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+        registration_page.click_registration_button()
+
+        dashboard_page.toolbar_view.check_visible()
+        dashboard_page.navbar.check_visible("username")
+        dashboard_page.sidebar.check_visible()
+        dashboard_page.sidebar.click_logout()
+
+        login_page.login_form.fill(email="user.name@gmail.com", password="password")
+        login_page.click_login_button()
+
+        # Проверка элементов Dashboard после входа
+        dashboard_page.toolbar_view.check_visible()
+        dashboard_page.navbar.check_visible("username")
+        dashboard_page.sidebar.check_visible()
+
     @pytest.mark.parametrize(
         "email, password",
         [
