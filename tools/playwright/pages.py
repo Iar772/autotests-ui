1504ae96c27e5dd6bd343a3411804d2b85246b58
Iaroslav Bulimov <iaroslavbulimov@192.168.100.5>
2026-03-04 18:03:49 +0300

    Работаем с моками в Playwright

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -3,6 +3,7 @@ import allure
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser  # Импортируем настройки
+from tools.playwright.mocks import mock_static_resources
 
 
 def initialize_playwright_page(
@@ -21,6 +22,7 @@ def initialize_playwright_page(
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
